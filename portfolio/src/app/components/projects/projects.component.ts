66261e1148ee619e6b54491e628756da8a29ddf4
Natalia392 <ntorrejonvalencia@gmail.com>
2023-11-09 18:19:03 -0300

    feat: implement data service to get project data in projects component

--- portfolio/src/app/components/projects/projects.component.ts
+++ portfolio/src/app/components/projects/projects.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Project } from 'src/app/interfaces/data';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
+
+  projects: Project[] = [];
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.projects = this.dataService.getProjects();
+  }
 
 }
