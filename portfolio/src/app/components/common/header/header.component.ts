77daadb157b7edbef7b52762172e0225f544bf01
Natalia392 <ntorrejonvalencia@gmail.com>
2023-11-16 20:02:00 -0300

    feat: don't show buttons if screen is small and show small nav instead

--- portfolio/src/app/components/common/header/header.component.ts
+++ portfolio/src/app/components/common/header/header.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-  constructor(private router: Router) {}
+export class HeaderComponent implements OnInit {
+  constructor(private router: Router, private breakpointObserver: BreakpointObserver) {}
 
   keyboardIconSrc = '../../assets/img/general-img/keyboard-icon.png'
+
+  isSmallScreen = false;
+  showButtons = true;
+
+  ngOnInit() {
+    this.breakpointObserver.observe([Breakpoints.XSmall, Breakpoints.Small]).subscribe(result => {
+      this.isSmallScreen = result.matches;
+      this.showButtons = !result.matches;
+    });
+  }
 
 }
